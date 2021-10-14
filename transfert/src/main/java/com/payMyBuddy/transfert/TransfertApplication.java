590ba6c228ddcfae98709370f2e110d22e5f8f4c
w-boar <80397826+w-boar@users.noreply.github.com>
2021-10-14 08:59:47 +0200

    Initial commit

---
+++ transfert/src/main/java/com/payMyBuddy/transfert/TransfertApplication.java
@@ -0,0 +1,13 @@
+package com.payMyBuddy.transfert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfertApplication.class, args);
+	}
+
+}
