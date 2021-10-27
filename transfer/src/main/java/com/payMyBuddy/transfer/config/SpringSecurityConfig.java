1009245e5bbff3baceaa43bc87772ebfa4a6cedd
w-boar <av.w.boar@gmail.com>
2021-10-27 22:24:13 +0200

    html+css+bootstrap

--- transfer/src/main/java/com/payMyBuddy/transfer/config/SpringSecurityConfig.java
+++ transfer/src/main/java/com/payMyBuddy/transfer/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,23 +17,26 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("springUser").password(passwordEncoder().encode("user123")).roles("USER")
+                .withUser("user@spring.omg").password(passwordEncoder().encode("user123")).roles("USER")
                 .and()
-                .withUser("springAdmin").password(passwordEncoder().encode("admin123")).roles("ADMIN", "USER");
+                .withUser("admin@spring.omg").password(passwordEncoder().encode("admin123")).roles("ADMIN", "USER");
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/css/**").permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
